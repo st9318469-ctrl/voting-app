0dbc345db2cb96484ef8e1d2a24df6018717c904
SACHINTHAKOR12 <rt566819@gmail.com>
2026-08-12 05:12:56 +0530

    final update

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const path = require('path');
 
-require('dotenv').config({ path: path.join(__dirname, '.env'), override: true });
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 
 const mongoURL = process.env.MONGODB_URL;
 
--- server.js
+++ server.js
@@ -52,7 +52,7 @@ if (fs.existsSync(frontendDistPath)) {
 
 
 
-const server = app.listen(PORT, '127.0.0.1',()=>{
+const server = app.listen(PORT, '0.0.0.0',()=>{
   console.log(`listening to the port ${PORT}`);
 });
 
